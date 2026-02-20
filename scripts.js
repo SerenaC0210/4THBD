a6b7dc282e5bffb553f3b23b051775ee0c22ade5
SerenaC0210 <serenaychen2005@gmail.com>
2026-02-19 23:11:00 -0500

    fixed nav bar, need to add spacing between the elements tho

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 function dropDown() { 
-    document.getElementById("dropLetters").classList.toggle("show");
+    event.preventDefault();
+    document.getElementById("myDropdownLetters").classList.toggle("show");
 }
 
 window.onclick = function(event) { 
     if(!event.target.matches('.dropbtn')) { 
-        var dropLetters = document.getElementById("dropLetters");
-        if(dropLetters.classList.contains('show')) { 
-            dropLetters.classList.remove('show');
+        var dropLetters = document.getElementsByClassName("dropdown-content");
+        var i;
+        for(i = 0; i < dropLetters.length; i++) { 
+            var openDropdown = dropLetters[i];
+            if(openDropdown.classList.contains('show')) { 
+                openDropdown.classList.remove('show');
+            }
         }
     }
 }
